2672390a0358356cee75ac80ecd44e92963c88b2
GiftsonVasanth <agiftson@gmail.com>
2022-01-11 00:28:07 -0600

    PW_4 Exercise using JSON

---
+++ PW_4/PW4_Files/PW_4.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  $.ajax({
+
+		 url: "PW_4.xml",
+		 dataType: "xml",
+		 success: function(data) {
+		 	  var title="",genre=[],director ="",cast="",synopsis="",score="";
+		    
+		    
+		    $("table").append('<tr><td>Title</td><td>Genre</td><td>Director</td><td>Cast</td><td>Short description</td><td>IMDB-rating</td></</tr>');
+		    $(data).find('playlist').each(function(){
+
+		      $(this).find('movie').each(function(){
+							title = $(this).find('title').text();
+
+							genre=[];
+							
+							$(this).find('genre').each(function(i,v){
+
+									genre.push($(v).text());
+							});
+							genre.join(", ")
+
+							director = $(this).find('director').text();
+
+							cast=[];
+
+							$(this).find('cast').each(function(i,v){
+
+        			$(v).find('person').each(function(i,v){
+
+        		 			cast.push($(v).attr('name'));
+
+							});
+							cast.join(", ")
+					});
+
+							$(this).find('imdb-info').each(function(){
+
+                 synopsis = $(this).find('synopsis').text();
+                 score = $(this).find('score').text();
+							});
+
+			  var info = '<tr><td>' + title +'</td><td>' + director +'</td><td>' +  genre + '</td><td>' +  cast + '</td><td>' +  synopsis + '</td><td>' +  score + '</td></tr>';
+				$("table").append(info);
+
+      	});
+
+      
+
+       });
+
+		   
+     },
+
+     error: function() { alert("error loading file");  }
+
+
+
+
+				
+		    
+		    
+
+		 
+		 
+    
+
+
+});
+});
+
